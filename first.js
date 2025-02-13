8a1281cb275579670e89ae09a7ab039ea346b28b
SACHIN88190 <sachinchoudhary88190@gmail.com>
2025-02-13 21:39:04 +0530

    payment add

--- first.js
+++ first.js
@@ -2,3 +2,7 @@ const button="Added a button";
 console.log(button);
 
 const login ="login page added";
+console.log(login);
+
+const payment ="Integarated the payment gatemay";
+console.log(payment);
